b43fc23f70d74de041eadafecd7c69022d1679c5
mustafai <musstafayilmaz96@gmail.com>
2024-07-22 15:13:06 +0300

    some CRUD operations for post and comment

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from storeapi.routers.post import router as posts_router
 
 app = FastAPI()
+app.include_router(posts_router, tags=["posts"])
+
 
 @app.get("/")
 async def root():
