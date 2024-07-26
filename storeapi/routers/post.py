1ca8f41ac5dff127e7c45988d5d8f2846d76814e
mustafai <musstafayilmaz96@gmail.com>
2024-07-26 15:55:54 +0300

    logging-router

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -1,4 +1,7 @@
+import logging
+
 from fastapi import APIRouter, HTTPException
+import logging
 from storeapi.database import comment_table, database, post_table
 from storeapi.models.post import (
     Comment,
@@ -10,9 +13,13 @@ from storeapi.models.post import (
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
+
 
 async def find_post(post_id: int):
+    logger.info(f"Finding post with id: {post_id}")
     query = post_table.select().where(post_table.c.id == post_id)
+    logger.debug(query)
     return await database.fetch_one(query)
 
 
@@ -26,14 +33,19 @@ async def create_post(post: UserPostIn):
 
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
+    logger.info("Getting all posts")
     query = post_table.select()
+    logger.debug(query)
     return await database.fetch_all(query)
 
 
 @router.post("/comment", response_model=Comment, status_code=201)
 async def create_comment(comment: CommentIn):
+    logger.info("Creating comment")
+
     post = await find_post(comment.post_id)
     if not post:
+        logger.error(f"Post not found id: {comment.post_id}")
         raise HTTPException(status_code=404, detail="Post not found")
 
     data = comment.model_dump()  # previously .dict()
@@ -44,14 +56,18 @@ async def create_comment(comment: CommentIn):
 
 @router.get("/post/{post_id}/comment", response_model=list[Comment])
 async def get_comments_on_post(post_id: int):
+    logger.info("Getting comments on post")
     query = comment_table.select().where(comment_table.c.post_id == post_id)
+    logger.debug(query)
     return await database.fetch_all(query)
 
 
 @router.get("/post/{post_id}", response_model=UserPostWithComments)
 async def get_post_with_comments(post_id: int):
+    logger.info("Getting post with comments")
     post = await find_post(post_id)
     if not post:
+        logger.error(f"Post not found with id: {post_id}")
         raise HTTPException(status_code=404, detail="Post not found")
 
     return {
